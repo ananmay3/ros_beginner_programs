fdc174bf009d7e9a086053eafe967a65014661b0
Ananmay Jain <ananmay.jain@gmail.com>
2018-09-10 03:03:52 -0500

    Added package for basic topic, service, and action code

--- catkin_ws/build/basics/CMakeFiles/_basics_generate_messages_check_deps_TimerResult.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/_basics_generate_messages_check_deps_TimerResult"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/_basics_generate_messages_check_deps_TimerResult.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
